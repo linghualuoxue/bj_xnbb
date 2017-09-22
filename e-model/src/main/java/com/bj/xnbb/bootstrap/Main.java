2cb943ac55fd70987d4cdb47ecef5a8080ce8113
Administrator <ylhi@qq.com>
2017-09-22 08:20:50 +0800

    创建项目

---
+++ e-model/src/main/java/com/bj/xnbb/bootstrap/Main.java
@@ -0,0 +1,10 @@
+package com.bj.xnbb.bootstrap;
+
+/**
+ * Created by Administrator on 2017/9/22.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("234");
+    }
+}
