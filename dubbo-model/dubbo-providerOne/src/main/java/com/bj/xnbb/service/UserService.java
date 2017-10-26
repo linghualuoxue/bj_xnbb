da2898b8caa1bd9d2c974a3393fb65d3780e800a
linghualuoxue <yangli@qq.com>
2017-10-26 11:57:05 +0800

    创建dubbo项目

---
+++ dubbo-model/dubbo-providerOne/src/main/java/com/bj/xnbb/service/UserService.java
@@ -0,0 +1,42 @@
+package com.bj.xnbb.service;
+
+
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import com.bj.xnbb.entity.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/userservice")
+@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8,ContentType.TEXT_XML_UTF_8})
+public interface UserService {
+
+    @GET
+    @Path("/testGet")
+    public void testGet();
+
+    @GET
+    @Path("/getUser")
+    public User getUser();
+
+    @GET
+    @Path("/get/{id:\\d+}")
+    public User getUser(@PathParam("id")Integer id);
+
+    @GET
+    @Path("/get/{id:\\d+}/{name:[a-zA-Z][0-9]}")
+    public User getUser(@PathParam("id")Integer id,@PathParam("name")String name);
+
+    @POST
+    @Path("/testPost")
+    public void testPost();
+
+    @POST
+    @Path("/postUser")
+    public User postUser();
+
+    @POST
+    @Path("/post/{id}")
+    public User postUser(@PathParam("id")String id);
+}
---
+++ dubbo-model/dubbo-providerOne/src/main/java/com/bj/xnbb/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.bj.xnbb.service.impl;
+
+import com.bj.xnbb.entity.User;
+import com.bj.xnbb.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    public void testGet() {
+
+    }
+
+    public User getUser() {
+        return null;
+    }
+
+    public User getUser(Integer id) {
+        return null;
+    }
+
+    public User getUser(Integer id, String name) {
+        return null;
+    }
+
+    public void testPost() {
+
+    }
+
+    public User postUser() {
+        return null;
+    }
+
+    public User postUser(String id) {
+        return null;
+    }
+}
