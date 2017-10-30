3e1357acb21fb6b151de49d1bcb688ae20099548
linghualuoxue <yangli@qq.com>
2017-10-30 18:15:21 +0800

    代码修改

--- xnbb-parent/xnbb-sys/src/main/java/xnbb/sys/web/SysIndexController.java
+++ xnbb-parent/xnbb-sys/src/main/java/xnbb/sys/web/SysIndexController.java
@@ -2,7 +2,12 @@ package xnbb.sys.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import xnbb.sys.entity.SysUser;
 import xnbb.sys.faced.SysUserFaced;
 
 @Controller
@@ -13,8 +18,8 @@ public class SysIndexController {
     private SysUserFaced sysUserFaced;
 
     @RequestMapping("getUser")
-    public SysUserFaced getUser(){
-          return sysUserFaced;
+    public @ResponseBody SysUser getUser(){
+          return sysUserFaced.getUser();
     }
 
 }
